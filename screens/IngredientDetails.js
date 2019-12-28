de9d345ccda54b15dd652df0013e7e18dcd55a6b
Hitesh Kumar <hitesh25kumar@gmail.com>
2019-12-28 15:35:46 +0530

    tutorial 17 ingredients styles

--- screens/IngredientDetails.js
+++ screens/IngredientDetails.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text, FlatList, TouchableHighlight, Image} from 'react-native';
+import {View, Text, FlatList, TouchableHighlight, Image,StyleSheet,Dimensions} from 'react-native';
 import {getAllIngredients} from '../mockapi';
+
+const { width, height } = Dimensions.get('window');
 
 export default class IngredientDetails extends Component {
   constructor(props) {
@@ -11,10 +13,10 @@ export default class IngredientDetails extends Component {
   renderIngredient = ({item}) => {
     return (
       <TouchableHighlight>
-        <View>
-          <Image source={{uri: item[0].photo_url}} />
-          <Text>{item[0].name}</Text>
-          <Text>{item[1]}</Text>
+        <View style={styles.container}>
+          <Image style={styles.photo} source={{uri: item[0].photo_url}} />
+          <Text style={styles.title}>{item[0].name}</Text>
+          <Text style={{color:'grey'}}>{item[1]}</Text>
         </View>
       </TouchableHighlight>
     );
@@ -39,3 +41,26 @@ export default class IngredientDetails extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    margin:10,
+    marginTop:30,
+    width:width / 3 - 30,
+    height:160
+  },
+  photo:{
+    width:width / 3 - 30,
+    height:100,
+    borderRadius:60
+  },
+  title:{
+    margin: 10,
+    marginBottom:5,
+    color:'#000',
+    fontSize:14,
+    textAlign:'center'
+  }
+})
